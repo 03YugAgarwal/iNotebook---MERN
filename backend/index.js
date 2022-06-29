599ebcd62b315492852b28ec063e7fa09b219d16
Yug <03yugagarwal@gmail.com>
2022-06-30 01:19:12 +0530

    added schemas to backend

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>[
-    res.send("Heyo")
-])
+
+//Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port,()=>{
     console.log("Backend server Started succesfully at http://localhost:3000");
 })
